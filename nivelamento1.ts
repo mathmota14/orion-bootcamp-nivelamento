65fa26da1149fabaccbfd64bb6df44041721d59d
Matheus Mota <92896528+mathmota14@users.noreply.github.com>
2024-10-03 20:31:01 -0300

    fix: revisão de código

--- nivelamento1.ts
+++ nivelamento1.ts
@@ -1,37 +1,33 @@
 //Letra a) utilizando palavra via parâmetro
-function contarVogaisParametro (texto: string): number {
-    let listaLetras = texto.split('');
-    let vogais: string[] = ["a", "e", "i", "o", "u"];
+function contarVogaisParametro(texto: string): number {
+    const listaLetras = texto.split('');
+    const vogais: string[] = ["a", "e", "i", "o", "u", "á", "à", "ã", "â", "é", "ê", "í", "ó", "õ", "ô", "ú"];
     let contagem = 0;
 
     for (let i = 0; i < listaLetras.length; i++) {
-        let letra = listaLetras[i].toLowerCase()
+        const letra = listaLetras[i].toLowerCase()
 
         if (vogais.includes(letra)) {
-            contagem += 1;
+            contagem++;
         }
     }
-
     return contagem
 }
-console.log(contarVogaisParametro("abacaxi"))
 
 //Letra b) contando a quantidade de vogais a partir do campo de formulário com o id dele sendo "idInputFormulario"
 //A função é adicionada no evento onclick do botão do formulário
-function contarVogaisFormulario(): void {
-    let textoDigitado = document.querySelector('#idInputFormulario')?.value;
-    let listaLetras = textoDigitado.split('');
-    let vogais: string[] = ["a", "e", "i", "o", "u"];
+function contarVogaisFormulario(): number {
+    const textoDigitado = document.querySelector('#idInputFormulario')?.value;
+    const listaLetras = textoDigitado.split('');
+    const vogais: string[] = ["a", "e", "i", "o", "u", "á", "à", "ã", "â", "é", "ê", "í", "ó", "õ", "ô", "ú"];
     let contagem = 0;
     
     for (let i = 0; i < listaLetras.length; i++) {
-        let letra = listaLetras[i].toLowerCase();
+        const letra = listaLetras[i].toLowerCase();
         
         if (vogais.includes(letra)) {
-            contagem += 1;
+            contagem++;
         }
     }
     alert(contagem)
-    
 }
-
